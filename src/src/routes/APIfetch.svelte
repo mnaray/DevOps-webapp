513cb06e713c2e299686985a854b91b5c8a0ef41
n¿co <nico.delvecchio@gmx.ch>
2023-09-14 10:30:40 +0200

    add apifetch func

---
+++ src/src/routes/APIfetch.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let triviaQuestions: any[] = [];
+
+	onMount(async () => {
+		const category = 'general';
+		const apiKey = '';
+
+		try {
+			const response = await fetch(
+				`https://api.api-ninjas.com/v1/trivia?category=${category}&limit=10`,
+				{
+					headers: {
+						'X-Api-Key': apiKey
+					}
+				}
+			);
+			const data = await response.json();
+			triviaQuestions = data;
+		} catch (error) {
+			console.error('Fehler beim Abrufen der Fragen:', error);
+		}
+	});
+</script>
+
+<div>
+	<div class="trivia-questions">
+		{#each triviaQuestions as question}
+			<div class="question-card">
+				<p>{question.question}</p>
+				<details>
+					<summary>Show Answer</summary>
+					<p>{question.answer}</p>
+				</details>
+			</div>
+		{/each}
+	</div>
+
+	<script>
+		function refreshPage() {
+			window.location.reload();
+		}
+	</script>
+</div>
+
+<style>
+	.trivia-questions {
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	.question-card {
+		border: 1px solid #ccc;
+		padding: 10px;
+		margin: 10px;
+		border-radius: 5px;
+	}
+</style>
