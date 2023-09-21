7c6ff669d68df8b21cde0c72733fcb656c119241
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-09-21 09:34:44 +0200

    add regex for devops

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -4,3 +4,8 @@ test('about page has expected h1', async ({ page }) => {
 	await page.goto('/about');
 	await expect(page.getByRole('heading', { name: 'About this app' })).toBeVisible();
 });
+
+test('home page contains DevOps text', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByText(/DevOps/)).toBeVisible();
+});
